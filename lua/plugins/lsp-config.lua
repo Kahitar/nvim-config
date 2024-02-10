c0ff50878a172d25072c7a02cd3c5fb5425800f5
🛰 Niklas Arens <niklas.arens@mercedes-benz.com>
2024-02-10 20:13:08 +0100

    feat: golang lsp setup

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -8,7 +8,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "tsserver", "pyright" }
+                ensure_installed = { "lua_ls", "tsserver", "pyright", "gopls" }
             })
         end
     },
@@ -19,9 +19,11 @@ return {
             lspconfig.lua_ls.setup({})
             lspconfig.tsserver.setup({})
             lspconfig.pyright.setup({})
+            lspconfig.gopls.setup({})
 
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set('n', 'gu', vim.lsp.buf.references, {})
             vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
         end
     }
