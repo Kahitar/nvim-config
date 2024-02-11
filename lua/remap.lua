f8c819774668361bba92d498d4a8bc7cc41a86db
Niklas Arens <niklas.arens.23@gmail.com>
2024-02-11 19:29:37 +0100

    chore: more prime remaps and which-key moved to separate file

--- lua/remap.lua
+++ lua/remap.lua
@@ -2,6 +2,16 @@
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
+-- swap lines
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Navigating findings windows (what is it called again) and (what's l?)
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
 -- yank into system clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
@@ -9,7 +19,13 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 -- delete without yanking
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
--- I have no idea what this does, but prime has it so its cool
-vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
-end)
+-- Search
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+---
+-- blindly copied from Primeagen, no idea what these do
+--
+
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
