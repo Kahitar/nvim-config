b459b7ffc89f2788eb91817d2313618fd0d5d5ad
🛰 Niklas Arens <niklas.arens@mercedes-benz.com>
2024-03-11 16:05:13 +0100

    chore: switch harpoon and findings window keybindings

--- lua/remap.lua
+++ lua/remap.lua
@@ -1,3 +1,4 @@
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- scroll half page and center cursor in buffer
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
@@ -7,8 +8,8 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Navigating findings windows (what is it called again) and (what's l?)
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
@@ -23,10 +24,12 @@ vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "#", "#zz")
 
+-- 
+vim.keymap.set("n", "<leader>q", "<cmd>%!jq .<CR>")
+
 ---
 -- blindly copied from Primeagen, no idea what these do
 --
 
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
