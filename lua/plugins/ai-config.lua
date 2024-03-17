584c70b5a8d8a6ef9c6e2d97779732dcf748417b
Niklas Arens <niklas.arens.23@gmail.com>
2024-03-17 12:10:25 +0100

    feat: use gpt4 instead 3.5-turbo

--- lua/plugins/ai-config.lua
+++ lua/plugins/ai-config.lua
@@ -188,7 +188,7 @@ return {
                     n = 1,
                 },
                 openai_edit_params = {
-                    model = "gpt-3.5-turbo",
+                    model = "gpt-4",
                     frequency_penalty = 0,
                     presence_penalty = 0,
                     temperature = 0,
